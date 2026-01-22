49b7395193795d74b86ba7d822b9f73a3a6e1591
Daren Tan <daren28@gmail.com>
2026-01-22 12:57:10 +0800

    Implement Greeting + Exit

--- src/main/java/Bloop.java
+++ src/main/java/Bloop.java
@@ -1,5 +1,10 @@
 public class Bloop {
     public static void main(String[] args) {
-        System.out.println("Hello from Bloop");
+        System.out.println("--------------------------------");
+        System.out.println("Hello I'm Bloop!");
+        System.out.println("What can I do for you?");
+        System.out.println("--------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("--------------------------------");
     }
 }
