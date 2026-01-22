08725bc086e38b793ab4401136f2b0da5937b82c
Daren Tan <daren28@gmail.com>
2026-01-22 13:05:11 +0800

    Echo User Commands

--- src/main/java/Bloop.java
+++ src/main/java/Bloop.java
@@ -4,7 +4,19 @@ public class Bloop {
         System.out.println("Hello I'm Bloop!");
         System.out.println("What can I do for you?");
         System.out.println("--------------------------------");
+
+        String userInput = System.console().readLine();
+
+        while (!userInput.equals("bye")) {
+            System.out.println("--------------------------------");
+            System.out.println("" + userInput);
+            System.out.println("--------------------------------");
+            userInput = System.console().readLine();
+        }
+
+        System.out.println("--------------------------------");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("--------------------------------");
+        
     }
 }
