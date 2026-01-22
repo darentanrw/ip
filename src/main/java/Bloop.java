a65668d0b30fc6224138f66af05a59066b0f5a61
Daren Tan <daren28@gmail.com>
2026-01-22 13:06:58 +0800

    Add indentations for clearer user input

--- src/main/java/Bloop.java
+++ src/main/java/Bloop.java
@@ -1,22 +1,22 @@
 public class Bloop {
     public static void main(String[] args) {
-        System.out.println("--------------------------------");
-        System.out.println("Hello I'm Bloop!");
-        System.out.println("What can I do for you?");
-        System.out.println("--------------------------------");
+        System.out.println("\t--------------------------------");
+        System.out.println("\tHello I'm Bloop!");
+        System.out.println("\tWhat can I do for you?");
+        System.out.println("\t--------------------------------");
 
         String userInput = System.console().readLine();
 
         while (!userInput.equals("bye")) {
-            System.out.println("--------------------------------");
-            System.out.println("" + userInput);
-            System.out.println("--------------------------------");
+            System.out.println("\t--------------------------------");
+            System.out.println("\t" + userInput);
+            System.out.println("\t--------------------------------");
             userInput = System.console().readLine();
         }
 
-        System.out.println("--------------------------------");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("--------------------------------");
+        System.out.println("\t--------------------------------");
+        System.out.println("\tBye. Hope to see you again soon!");
+        System.out.println("\t--------------------------------");
         
     }
 }
