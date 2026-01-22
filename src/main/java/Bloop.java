53e1ab58ecf5cf4b515b91169bcfc8782156e4e1
Daren Tan <daren28@gmail.com>
2026-01-22 17:05:39 +0800

    Refactor Bloop to reduce duplicate code

--- src/main/java/Bloop.java
+++ src/main/java/Bloop.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 public class Bloop {
     public static void printLine() {
         System.out.println("\t--------------------------------");
@@ -6,12 +7,7 @@ public class Bloop {
 
     public static void main(String[] args) {
         ArrayList<Task> taskList = new ArrayList<>();
-
-        printLine();
-        System.out.println("\tHello I'm Bloop!");
-        System.out.println("\tWhat can I do for you?");
-        printLine();
-
+        System.out.println("\t--------------------------------\n\tHello I'm Bloop!\n\tWhat can I do for you?\n\t--------------------------------");
         String userInput = System.console().readLine();
 
         while (!userInput.equals("bye")) {
@@ -26,14 +22,22 @@ public class Bloop {
                     }
                 }
             } else {
-                String[] userInputArray = userInput.split(" ");
+                String[] userInputArray = userInput.split(" ", 2);
+                String[] validCommands = {"mark", "unmark", "deadline", "event", "todo"};
+
+                if (userInputArray.length < 2 || !List.of(validCommands).contains(userInputArray[0])) {
+                    System.out.println("\tBloop bloop.... Seems like you have entered an invalid command!");
+                    printLine();
+                    userInput = System.console().readLine();
+                    continue;
+                } 
                 String command = userInputArray[0];
                 String userQuery = userInputArray[1];
 
                 if (command.equals("mark") || command.equals("unmark")) {
                     int taskIndex = Integer.parseInt(userQuery) -1;
                     if (taskIndex < 0 || taskIndex >= taskList.size()) {
-                        System.out.println("\tInvalid task index");
+                        System.out.println("\tInvalid task index.");
                     } else {
                         taskList.get(taskIndex).setStatus(command == "mark" ? true : false);
                         System.out.println("\tNice! I have marked this task as " + (command == "mark" ? "done" : "not done") + ":");
@@ -41,34 +45,28 @@ public class Bloop {
                     }
                 }
 
-                else if (command.equals("deadline")) {
-                    String[] userQueryArray = userQuery.split(" /by ");
-                    taskList.add(new DeadlineTask(userQueryArray[0], userQueryArray[1]));
+                else {
+                    if (command.equals("deadline")) {    
+                        String[] userQueryArray = userQuery.split(" /by ");
+                        taskList.add(new DeadlineTask(userQueryArray[0], userQueryArray[1]));
+                    } else if (command.equals("event")) {
+                        String[] userQueryArray = userQuery.split("\\s+/from\\s+|\\s+/to\\s+");
+                        taskList.add(new EventTask(userQueryArray[0], userQueryArray[1], userQueryArray[2]));
+                    } else if (command.equals("todo")) {
+                        taskList.add(new ToDoTask(userQuery));
+                    }
+                    System.out.println("\tGot it. I've added this task: ");
                     System.out.println("\t\t" + taskList.get(taskList.size() - 1).toString());
                     System.out.println("\tYou now have " + taskList.size() + " tasks in the list.");
-                    
-                } else if (command.equals("event")) {
-                    String[] userQueryArray = userQuery.split("\\s+/from\\s+|\\s+/to\\s+");
-                    taskList.add(new EventTask(userQueryArray[0], userQueryArray[1], userQueryArray[2]));
-                    System.out.println("\t\t" + taskList.get(taskList.size() - 1).toString());
-                    System.out.println("\tYou now have " + taskList.size() + " tasks in the list.");
-                
-                } else if (command.equals("todo")) {
-                    taskList.add(new ToDoTask(userQuery));
-                    System.out.println("\t\t" + taskList.get(taskList.size() - 1).toString());
-                    System.out.println("\tYou now have " + taskList.size() + " tasks in the list.");
-
-                } else {
-                    System.out.println("\tBloop bloop.... Seems like you have entered an invalid command!");
                 }
             }
             printLine();
             userInput = System.console().readLine();
         }
 
-        System.out.println("\t--------------------------------");
+        printLine();
         System.out.println("\tBye. Hope to see you again soon!");
-        System.out.println("\t--------------------------------");
+        printLine();
         
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -13,8 +13,6 @@ public class Task {
     this.taskName = taskName;
     this.isDone = false;
     this.type = type;
-
-    System.out.println("\tGot it. I've added this task: ");
 }
 
   public void setStatus(boolean isDone) {
