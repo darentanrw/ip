f3362cf64e19f6693d4ebf3291c58dc918e588e2
Daren Tan <daren28@gmail.com>
2026-01-22 13:17:13 +0800

    Implement Add + List Logic

--- src/main/java/Bloop.java
+++ src/main/java/Bloop.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
 public class Bloop {
     public static void main(String[] args) {
+        ArrayList<String> taskList = new ArrayList<>();
+
         System.out.println("\t--------------------------------");
         System.out.println("\tHello I'm Bloop!");
         System.out.println("\tWhat can I do for you?");
@@ -8,9 +11,19 @@ public class Bloop {
         String userInput = System.console().readLine();
 
         while (!userInput.equals("bye")) {
-            System.out.println("\t--------------------------------");
-            System.out.println("\t" + userInput);
-            System.out.println("\t--------------------------------");
+
+            if (userInput.equals("list")) {
+                System.out.println("\t--------------------------------");
+                for (int i = 0; i < taskList.size(); i++) {
+                    System.out.println("\t" + (i+1) + ". " + taskList.get(i));
+                }
+                System.out.println("\t--------------------------------");
+            } else {
+                System.out.println("\t--------------------------------");
+                taskList.add(userInput);
+                System.out.println("\t added: " + userInput);
+                System.out.println("\t--------------------------------");
+            }
             userInput = System.console().readLine();
         }
 
