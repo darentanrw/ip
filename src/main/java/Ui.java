7dfa64ed05f5fceaad2ac34866fccf2f96998642
Daren Tan <daren28@gmail.com>
2026-02-10 08:02:00 +0800

    Refactor UI to use instance methods

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,55 +1,56 @@
-import java.util.ArrayList;
-
+/**
+ * Handles all interactions with the user (reading input, printing output).
+ */
 public class Ui {
-    public static void printWelcomeMessage() {
+    public void printWelcomeMessage() {
         System.out.println("\t--------------------------------\n\tHello I'm Bloop!\n\tWhat can I do for you?\n\t--------------------------------");
     }
 
-    public static void printGoodbyeMessage() {
+    public void printGoodbyeMessage() {
         printLine();
         System.out.println("\tBye. Hope to see you again soon!");
         printLine();
     }
 
-    public static void printLine() {
+    public void printLine() {
         System.out.println("\t--------------------------------");
     }
 
-    public static void printTaskList(ArrayList<Task> taskList) {
+    public void printTaskList(TaskList taskList) {
         if (taskList.size() == 0) {
             System.out.println("\tNo tasks added yet");
         } else {
             System.out.println("\tHere are the tasks in your list:");
             for (int i = 0; i < taskList.size(); i++) {
-                System.out.println("\t" + (i+1) + ". " + taskList.get(i).toString());
+                System.out.println("\t" + (i + 1) + ". " + taskList.getTask(i).toString());
             }
         }
     }
 
-    public static void taskUpdateMessage(Task task, String action) {
-        System.out.println("\t" + action); 
+    public void taskUpdateMessage(Task task, String action) {
+        System.out.println("\t" + action);
         System.out.println("\t\t" + task.toString());
-        printLine();
     }
 
-    public static void newTaskMessage(Task task, int size) {
+    public void newTaskMessage(Task task, int size) {
         System.out.println("\tGot it. I've added this task: ");
         System.out.println("\t\t" + task.toString());
         printTaskListSize(size);
     }
 
-
-    public static void printInvalidCommandMessage() {
+    public void printInvalidCommandMessage() {
         System.out.println("\tBloop bloop.... Seems like you have entered an invalid command!");
-        printLine();
     }
 
-    public static void printTaskListSize(int size) {
+    public void printTaskListSize(int size) {
         System.out.println("\tYou now have " + size + " tasks in the list.");
     }
 
-    public static void printCustomErrorMessage(String errorMessage) {
+    public void printCustomErrorMessage(String errorMessage) {
         System.out.println("\t" + errorMessage);
-        printLine();
+    }
+
+    public void showLoadingError() {
+        System.out.println("\tError loading tasks from file.");
     }
 }
